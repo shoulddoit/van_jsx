93a4a2929228e7f8bff542f22b7a614f148f5301
yuekcc <yuekcc@qq.com>
2023-08-02 21:40:49 +0800

    增加例子

---
+++ examples/reactive/index.jsx
@@ -0,0 +1,23 @@
+import { h, render } from '../../';
+import van from 'vanjs-core';
+
+function DisplayNumber({ num }) {
+  return <h1 style={() => `color: ${num % 2 === 0 ? 'green' : 'red'}`}>{num}</h1>;
+}
+
+function App() {
+  const num = van.state(0);
+
+  function add() {
+    num.val += 1;
+  }
+
+  return (
+    <div>
+      {() => <DisplayNumber num={num.val}></DisplayNumber>}
+      <button onclick={add}>ADD</button>
+    </div>
+  );
+}
+
+render('#app', <App />);
